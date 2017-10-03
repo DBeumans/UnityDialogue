c269b0b61529fc5ed0e4b9f8d179006e50683b66
Danilo B <buncho.gamingnetwork@gmail.com>
2017-10-04 00:20:32 +0200

    Added project files, added XML loader, not tested, added basic player movement, added NPC base class.

---
+++ UnityDialogue/Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    private static Vector3 movementInput = new Vector2();
+
+    public static Vector3 Get_MovementInput { get { return movementInput; } }
+
+    private void Update()
+    {
+        movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"),0);
+    }
+}
---
+++ UnityDialogue/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed;
+
+    private Rigidbody rigidbody;
+
+    private Vector3 movement;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float horizontal = InputManager.Get_MovementInput.x;
+        float vertical = InputManager.Get_MovementInput.y;
+
+        movement = new Vector3(horizontal, 0, vertical);
+
+    }
+
+    private void FixedUpdate()
+    {
+        movement = movement * movementSpeed * Time.fixedDeltaTime;
+        rigidbody.MovePosition(rigidbody.position + movement);
+    }
+}
